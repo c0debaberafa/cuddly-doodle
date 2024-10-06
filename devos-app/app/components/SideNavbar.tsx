f7db303d58dbd0af01270670357a47e37534a3c4
marco <mvcantero1@up.edu.ph>
2024-10-06 22:30:11 +0800

    Added SideNavbar with tailwind for styling

--- devos-app/app/components/SideNavbar.tsx
+++ devos-app/app/components/SideNavbar.tsx
@@ -7,14 +7,14 @@ export default function SideNavbar() {
                         bg-gray-900 text-white shadow-lg">
             <SideBarIcon icon={<FaUser size="28"/>} text={"My Elections"} />
             <SideBarIcon icon={<FaVoteYea size="28"/>} text={"Enrolled Elections"} />
-            <SideBarIcon icon={<FaWallet size="28"/>} text={"Phantom Wallet"}/>
+            <SideBarIcon icon={<FaWallet size="28"/>} text={"Phantom Wallet"} isBottom/>
         </div>
     );
     
 }
 
-const SideBarIcon = ({ icon, text } : { icon:any, text:any}) => (
-    <div className="sidebar-icon group">
+const SideBarIcon = ({ icon, text, isBottom } : { icon:any, text:any, isBottom?: boolean}) => (
+    <div className={`sidebar-icon group ${isBottom ? 'mt-auto': ''}`}>
         {icon}
         <span className="sidebar-tooltip group-hover:scale-100">
             {text}
